059ef6bc6a69f98b682eefbee5ea26fadc9f7065
Joe Martin <throwstuff@joemart.in>
2021-04-01 11:08:45 -0400

    Update project.service.ts

--- my-app/src/app/project.service.ts
+++ my-app/src/app/project.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Project } from './project';
 import { PROJECTS } from './mock-projects';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
